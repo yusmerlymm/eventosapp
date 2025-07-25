043c6fb6e341518d5f21819af7fe9e1a10f63c88
JeanGalindez <jeangalindez21@gmail.com>
2025-07-24 20:55:27 -0400

    feat(db,backend): creada la  tabla events_type, creado el modelo EventsType y modificados los modelos Event y EventsCategory

---
+++ app-modules/events/database/migrations/2025_07_24_232750_create_events_type_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events_type', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_tipo_evento');
+            $table->foreignId('id_categoria')
+                  ->constrained('events_category')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events_type');
+    }
+};
